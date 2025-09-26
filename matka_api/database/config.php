53a1aaeee83a1618b8d93ee154df8673182129ea
falgunk36-a11y <falgunk36@gmail.com>
2025-09-26 05:46:29 +0530

    Update config.php

--- matka_api/database/config.php
+++ matka_api/database/config.php
@@ -1,8 +1,8 @@
 <?php
 define('DB_SERVER', 'localhost');
-define('DB_USER', 'add db user');
-define('DB_PASSWORD', 'add password');
-define('DB_NAME', 'add db name');
+define('DB_USER', 'raju khanna');
+define('DB_PASSWORD', 'admin@123');
+define('DB_NAME', 'utaam');
 
 try {
     $db = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_NAME, DB_USER, DB_PASSWORD);
